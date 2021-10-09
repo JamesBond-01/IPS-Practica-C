5049eb83e8b3f11d6ea5c7c45e6d2d927b6bb7da
LeandroGomez1 <leandrodanielgomez@gmail.com>
2021-10-09 13:21:17 -0300

    Resolucion ejercicio 3

--- Facultad/practica1/ejercicio3.c
+++ Facultad/practica1/ejercicio3.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <malloc.h>
+
+void invertedNum(int *pointNum) {
+	for (int i = 10; i > 0; i--) {
+		pointNum--;
+		printf("%d\n", *pointNum);
+	}
+}
+
 int main() {
 
 	int *pnum = (int*)malloc(10 * sizeof(int));
+	int *originPointer = pnum;
 
 	printf("Ingrese 10 numeros:\n");
 
@@ -12,11 +21,9 @@ int main() {
 	}
 
 	printf("Numeros inversos:\n");
-	for (int i = 10; i > 0; i--) {
-		pnum--;
-		printf("%d\n", *pnum);
-	}
+	invertedNum(pnum);
 
+	pnum = originPointer;
 	free(pnum);
 	pnum = NULL;
 
