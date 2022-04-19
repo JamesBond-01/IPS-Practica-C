2a73403dda267e9eca5b184faf55573e322488d1
LeandroGomez1 <leandrodanielgomez@gmail.com>
2022-04-19 17:38:48 -0300

    Finalizacion ejercicio peopleData

--- Facultad/practicasV2/practica 2: Estructuras, uniones y campos de bits/encontradasEnLaWeb/peopleData.c
+++ Facultad/practicasV2/practica 2: Estructuras, uniones y campos de bits/encontradasEnLaWeb/peopleData.c
@@ -3,56 +3,78 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define TOTAL_PEOPLE 5
-#define MAX_CHARACTERS 30
+#define TOTAL_PEOPLE 3
+#define MAX_CHARACTERS 10
 
-typedef struct {
+typedef struct person {
     char *name;
     char *surname;
     char sex;
     int age;
-    int telNumber;
+    long telNumber;
 } Person;
 
+Person *getData();
+
 int main() {
-    Person *people = (Person *)malloc(TOTAL_PEOPLE * sizeof(Person));
-
-    people->name = (char *) malloc(MAX_CHARACTERS * sizeof(char));
-    people->surname = (char *) malloc(MAX_CHARACTERS * sizeof(char));
-
-    Person *pPeople = people;
+    Person *people;
 
     printf("Por favor ingrese los datos de las siguientes %i personas.\n", TOTAL_PEOPLE);
+    people = getData();
+
+
+    printf("====================================\n");
+    printf("Datos ingresados\n");
+
     for(int i = 0; i < TOTAL_PEOPLE; i++) {
-        printf("Persona %i\n", i);
+        printf("Persona %i\n", i + 1);
+        printf("\tNombre: %s", people->name);
+        printf("\tApellido: %s", people->surname);
+        printf("\tEdad: %d\n", people->age);
+        printf("\tSexo: %c\n", people->sex);
+        printf("\tNumero de telefono: %ld\n", people->telNumber);
+
+        people ++;
+    }
+    free(people);
+
+    return 0;
+}
+
+Person *getData() {
+    int c;
+    Person *people = (Person *)malloc(TOTAL_PEOPLE * sizeof(Person));
+    Person *pPeople = people;
+
+    for(int i = 0; i < TOTAL_PEOPLE; i++) {
+        people->name = calloc(MAX_CHARACTERS, sizeof(char));
+        people->surname = calloc(MAX_CHARACTERS, sizeof(char));
+        printf("Persona %i\n", i + 1);
+
         printf("\tNombre: ");
         fgets(people->name, MAX_CHARACTERS, stdin);
+
         printf("\tApellido: ");
         fgets(people->surname, MAX_CHARACTERS, stdin);
+
         printf("\tEdad: ");
         scanf("%d", &people->age);
+
         printf("\tSexo [M/F]: ");
         scanf(" %c", &people->sex);
-        while((people->sex != 'f' || people->sex != 'F') || (people->sex != 'm' || people->sex != 'M')) {
+        while((people->sex != 'f' && people->sex != 'F') && (people->sex != 'm' && people->sex != 'M')) {
             printf("Ingrese el sexo correctamente [M/F]: ");
             scanf(" %c", &people->sex);
         }
+
         printf("\tNumero de telefono: ");
-        scanf("%d", &people->telNumber);
+        scanf("%ld", &people->telNumber);
 
-        people++;
+        while ((c = getchar()) != '\n' && c != EOF) { }
+
+        people ++;
     }
+
     people = pPeople;
-    printf("Datos ingresados\n");
-    for(int i = 0; i < TOTAL_PEOPLE; i++) {
-        printf("Persona %i\n", i);
-        printf("\tNombre: %s", people->name);
-        printf("\tApellido: %s", people->surname);
-        printf("\tEdad: %d", people->age);
-        printf("\tSexo: %c", people->sex);
-        printf("\tNumero de telefono: %d", people->telNumber);
-
-        people++;
-    }
-    return 0;
+    return people;
 }
