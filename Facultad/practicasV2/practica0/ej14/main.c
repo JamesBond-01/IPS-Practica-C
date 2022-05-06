a8ab0865f122646959891a0f80a77149d335897b
LeandroGomez1 <leandrodanielgomez@gmail.com>
2022-05-06 02:45:36 -0300

    Trabajos de manejo de archivos

--- Facultad/practicasV2/practica0/ej14/main.c
+++ Facultad/practicasV2/practica0/ej14/main.c
@@ -7,35 +7,53 @@
 
 int main() {
     int **matrix = (int **)malloc(MATRIX_SIZE * sizeof(int));
-    int diagonal[(int) sqrt(MATRIX_SIZE)];
+    int diagonalSize = sqrt(MATRIX_SIZE);
+    int *diagonal = (int *)malloc(diagonalSize * sizeof(int));
     int sum;
 
     srand(time(NULL));
-    for(int i = 0; i < (sqrt(MATRIX_SIZE)); i++) {
-        for(int j = 0; i < (sqrt(MATRIX_SIZE)); j++) {
+
+    for(int i = 0; i < diagonalSize; i++) {
+        matrix[i] = malloc(sizeof(int));
+        for(int j = 0; j < diagonalSize; j++) {
+            matrix[j] = malloc(sizeof(int));
+        }
+    }
+
+    for(int i = 0; i < diagonalSize; i++) {
+        for(int j = 0; j < diagonalSize; j++) {
             matrix[i][j] = 1 + rand() % 9;
         }
     }
     printf("Matriz original\n");
 
-    for(int i = 0; i < (sqrt(MATRIX_SIZE)); i++) {
+    for(int i = 0; i < diagonalSize; i++) {
         printf("\t");
-        for(int j = 0; j < (sqrt(MATRIX_SIZE)); j++) {
+        for(int j = 0; j < diagonalSize; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 
-    for(int i = 0; i < (sqrt(MATRIX_SIZE)); i++) {
-        int k = i;
-        diagonal[i] = matrix[i][k];
+    for(int i = 0; i < diagonalSize; i++) {
+        diagonal[i] = matrix[i][i];
     }
     sum = diagonal[0];
 
-    for(int i = 1; i < (sqrt(MATRIX_SIZE)); i++) {
+    for(int i = 1; i < diagonalSize; i++) {
         sum += diagonal[i];
     }
     printf("Traza de la matriz: %d\n", sum);
+
+    printf("Matriz transpuesta\n");
+
+    for(int i = 0; i < diagonalSize; i++) {
+        printf("\t");
+        for(int j = 0; j < diagonalSize; j++) {
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
