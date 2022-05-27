b2020d07d51ce3ae274c72298e88eb8692dfe34d
LeandroGomez1 <leandrodanielgomez@gmail.com>
2022-05-27 15:30:24 -0300

    Ejercicios de ayuda

--- Facultad/tda/pila/main.c
+++ Facultad/tda/pila/main.c
@@ -9,12 +9,12 @@ typedef struct Node {
 
 void push(int number);
 void pop();
-void print();
+void showList();
 int size();
 bool isEmpty();
 int last();
 
-node *superior = NULL;
+node *topNode = NULL;
 
 int main() {
 
@@ -22,7 +22,28 @@ int main() {
 }
 
 void push(int number) {
-    print("Agregando nuevo elemento...\n");
+    printf("Agregando nuevo elemento...\n");
     node *newNode = (node *)malloc(sizeof(node));
-
+    newNode->number = number;
+    newNode->next = topNode;
+    topNode = newNode;
+}
+void pop() {
+    printf("Eliminando el ultimo elemento...\n");
+    if(topNode == NULL) {
+        node *tmpNode = topNode;
+        topNode = topNode->next;
+        free(tmpNode);
+    }
+}
+void showList() {
+    node *tmpNode = topNode;
+    while(tmpNode != NULL) {
+        printf("\t%d\n", tmpNode->number);
+        tmpNode = tmpNode->next;
+    }
+}
+int size() {
+    int counter = 0;
+    if(topNode == NULL)
 }
